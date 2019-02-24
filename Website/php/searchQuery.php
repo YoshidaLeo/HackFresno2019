2cddac585186554e362e9bad0aedfc1d6b94ca60
Leonardo Yoshida <leonardo@drumwave.com>
2019-02-24 10:39:08 -0800

    First Commit

---
+++ Website/php/searchQuery.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+  <link rel="stylesheet" href="../styles/index.css">
+  </head>
+  <body>
+    <header>
+      <h1>Teacher Tech Finder</h1>
+    </header>
+    <br/>
+    <img src="../images/TTF.png" width="100" height="100">
+    <section>
+    <?php 
+    $servername = "localhost";
+    $username = "user1";
+    $password = "password";
+    $dbname = "demodb";
+    $where_clause = $_GET["variable"];
+    $temp = (string)$where_clause;
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    
+    $sql = "SELECT name, url, tags, rating FROM example WHERE (tags = $where_clause) OR (name = $where_clause) OR (url = $where_clause) OR (rating = $where_clause)";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "<div> Name: ". $row["name"]. "Website: " . $row["url"] . " - Rating: ".$row["rating"]. "<div/> <br/>";
+        }
+    } else {
+        echo "No result Found";
+    }
+    
+    $conn->close();
+    ?>
+    </section>
+  </body>
+</html>
---
+++ Website/php/surveyQuery.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+  <link rel="stylesheet" href="../styles/index.css">
+  </head>
+  <body>
+    <header>
+      <h1>Teacher Tech Finder</h1>      
+    </header>
+    <br/>
+    <img src="../images/TTF.png" width="100" height="100">
+    <section>
+    <?php 
+    $servername = "localhost";
+    $username = "user1";
+    $password = "password";
+    $dbname = "demodb";
+    $where_clause = $_GET["variable"];
+    
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    
+    $sql = "SELECT name, url, rating, description FROM example WHERE tags = $where_clause";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "<div> <strong>". $row["name"]. "</strong><br/>Website: " . $row["url"] . "<br/>Rating:  ".$row["rating"]. "&#9734"."<br/>Description:  ".$row["description"] ."<div/> <br/><br/>";
+        }
+    } else {
+        echo "0 results";
+    }
+    
+    $conn->close();
+    ?>
+    </section>
+  </body>
+</html>
